adfa8243612402e31fcefa8aa182762350b900f8
Clayton Hunter <claytonhunter10@gmail.com>
2025-11-23 21:33:00 -0800

    Fixed modal bug, enhanced the styling for the home page, navbar, notifications, and popup modal, added filtering on the home page

--- kairos/events/views.py
+++ kairos/events/views.py
@@ -11,8 +11,24 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required(login_url='login')
 def events(request):
-    events = Event.objects.all().order_by('-date')
-    print("DEBUG - Events fetched:", events)
+    search = request.GET.get("search")
+    sort = request.GET.get("sort")
+    events = Event.objects.all()
+    
+    if search != None:
+        events = events.filter(title__icontains=search)
+
+    if sort != None:
+        if sort == "most-recent":
+            events = events.order_by("date")
+        elif sort == "least-recent":
+            events = events.order_by("-date")
+        elif sort == "alphabetical-increasing":
+            events = sorted(events, key=lambda x: x.title)
+        elif sort == "alphabetical-decreasing":
+            events = sorted(events, key=lambda x: x.title, reverse=True)
+
+    #print("DEBUG - Events fetched:", events)
     return render(request, 'events/events.html', {'events': events})
 
 
